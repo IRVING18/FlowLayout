c70a83bc872cd3c605509877120c9e734587cfa2
王征 <1141502381@qq.com>
2019-03-23 19:24:13 +0800

    tablayout

--- app/src/main/java/com/layout/TagLayout.java
+++ app/src/main/java/com/layout/TagLayout.java
@@ -25,12 +25,7 @@ public class TagLayout extends ViewGroup {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int widthUsed  = 0;
-        int heightUsed = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-
-          /*  //拿到开发者xml的要求
+        /*  //拿到开发者xml的要求
             LayoutParams layoutParams = child.getLayoutParams();
             //获取当前viewGroup的模式
             int specWidthMode = MeasureSpec.getMode(widthMeasureSpec);
@@ -59,25 +54,59 @@ public class TagLayout extends ViewGroup {
                     break;
                 case LayoutParams.WRAP_CONTENT:
                     break;
-            }*/
-
-//           ==>
-
-            measureChildWithMargins(child, widthMeasureSpec, widthUsed, heightMeasureSpec, heightUsed);
-
-            //1、测量子view
+            }
             child.measure();
+            */
+
+//        这是就是 measureChildWithMargins()内部实现的思路
+
+        //横向当前行已使用的宽度
+        int lineWidthUsed  = 0;
+        //总高度
+        int allheightUsed = 0;
+        //所有行最高和最宽的值 , 可以作为tagView 的宽
+        int maxWidth  = 0;
+        int maxHeight  = 0;
+        //获取当前layoutview的mode和宽度
+        int tagLayoutSpecWidthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int tagLayoutSpecWidthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+
+            //1、方法作用：测量子view
+            //2、注意使用时需要重写generateLayoutParams方法，返回一个MarginLayoutParams
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, allheightUsed);
+            //1、UNSPECIFIED 的话就永远不用重新测绘childview，因为父view的宽度没有限制（处在horizonScrollView），那子view直接一直往后排就好了。
+            //2、如果已使用的宽度 + 当前测量的child宽度 > 当前Layout的宽度的话，就错行，再重新量一遍当前正在量的child。
+            //再量一次当前的childview是因为，上次测量如果父view宽度就不够，他就会压缩自己，这样就不准确了。
+            if (tagLayoutSpecWidthMode != MeasureSpec.UNSPECIFIED && lineWidthUsed + child.getMeasuredWidth() > tagLayoutSpecWidthSize) {
+                //当前行 置为头部
+                lineWidthUsed = 0;
+                allheightUsed += maxHeight;
+                maxHeight = 0;
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, allheightUsed);
+            }
 
             //拿到自view的尺寸，存起来
-            Rect rect = childrenBounds.get(i);
-            rect.set(widthUsed, heightUsed, widthUsed + child.getMeasuredWidth(), heightUsed + child.getMeasuredHeight());
+            Rect childBound;
+            if (childrenBounds.size() <= i) {
+                childBound = new Rect();
+                childrenBounds.add(childBound);
+            } else {
+                childBound = childrenBounds.get(i);
+            }
+            //存储当前childview的位置
+            childBound.set(lineWidthUsed, allheightUsed, lineWidthUsed + child.getMeasuredWidth(), allheightUsed + child.getMeasuredHeight());
 
-            widthUsed += child.getMeasuredWidth();
-
+            lineWidthUsed += child.getMeasuredWidth();
+            //存储最宽的值
+            maxWidth = Math.max(lineWidthUsed, maxWidth);
+            maxHeight = Math.max(maxHeight, child.getMeasuredHeight());
         }
         //保存当前view的尺寸
-        int width  = widthUsed;
-        int height = 0;
+        int width  = maxWidth;
+        int height = allheightUsed + maxHeight;
         setMeasuredDimension(width, height);
 
     }
@@ -92,5 +121,10 @@ public class TagLayout extends ViewGroup {
             Rect rect  = childrenBounds.get(i);
             child.layout(rect.left, rect.top, rect.right, rect.bottom);
         }
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
     }
 }
